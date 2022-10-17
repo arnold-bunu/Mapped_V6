66c2aa4de2b10f9781362a9df5e9f85cae355e84
Arnold Bunu <arnoldbunu@gmail.com>
2022-10-17 12:01:15 +0200

    fragment check

--- app/src/main/java/com/example/mapped_v6/MainActivity.java
+++ app/src/main/java/com/example/mapped_v6/MainActivity.java
@@ -4,15 +4,32 @@ package com.example.mapped_v6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mapped_v6.R;
 
+import ui.MapsFragment;
+
 public class MainActivity extends AppCompatActivity {
+    Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTest = findViewById(R.id.btnTest);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startActivity(new Intent(MainActivity.this, MapsFragment.class));
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/ui/MapsFragment.java
+++ app/src/main/java/ui/MapsFragment.java
@@ -2,14 +2,32 @@ package ui;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mapped_v6.R;
+import com.example.mapped_v6.databinding.FragmentMapsBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,44 +35,120 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener {
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    PlacesClient placesClient;
+    viewModel viewModel;
+    Location lastKnownLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    FragmentContainerView fragmentContainerView;
+    ConstraintLayout placeInformation;
+   FragmentMapsBinding binding;
+ Boolean locationPermissionGranted = false;
+     Button directionButton, favouriteButton;
+    TextView placeInfoTitle, placeInfoDuration, placeInfoDistance;
+
+    // VARIABLES USED TO GET LANDMARKS NEAR THE USERS LOCATION
+    Map<String, String> placeIds = new HashMap<String, String>();
+    String currentPlaceId;
+    String currentPlaceAddress;
+    String currentPlaceName;
+    Uri currentPlaceWebUri;
+   // GeoApiContext mGeoApiContext;
+   ArrayList<LatLng> mMarkerPoints;
+
 
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            map = googleMap;
+
+            if (locationPermissionGranted) {
+                if (ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                fetchlocation();
+            }
+
         }
     };
+
+    private void fetchlocation() {
+
+        if (map == null) {
+            return;
+        }  try {
+            map.getUiSettings().setMapToolbarEnabled(false);
+            if (locationPermissionGranted) {
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+                map.getUiSettings().setCompassEnabled(true);
+                map.getUiSettings().setZoomControlsEnabled(true);
+            } else {
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                map.getUiSettings().setCompassEnabled(false);
+                map.getUiSettings().setZoomControlsEnabled(false);
+                lastKnownLocation = null;
+                //getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        viewModel = new ViewModelProvider(this).get(viewModel.class);
+
+        binding = FragmentMapsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Places.initialize(getActivity().getApplicationContext(), getString(R.string.google_maps_key));
+        placesClient = Places.createClient(getContext());
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        fragmentContainerView = (FragmentContainerView) getView().findViewById(R.id.fragmentContainerViewPlaceInformation);
+
+        placeInformation = (ConstraintLayout) getView().findViewById(R.id.placeInformation);
+
+        placeInfoTitle = (TextView) getView().findViewById(R.id.placeInfoTitle);
+        directionButton = (Button) getView().findViewById(R.id.directionButton);
+        favouriteButton = (Button) getView().findViewById(R.id.addToFavouritesButton);
+        placeInfoDistance = (TextView) getView().findViewById(R.id.placeInfoDistance);
+        placeInfoDuration = (TextView) getView().findViewById(R.id.placeInfoDuration);
+
+
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+            mMarkerPoints = new ArrayList<>();
         }
     }
 
