b5e4753e6061361c6167c3abdfceb025222cfe24
Carla <carlakearney57@gmail.com>
2022-10-17 13:21:23 +0200

    fav and settings commit

--- app/src/main/java/ui/favourites/FavouritesFragment.java
+++ app/src/main/java/ui/favourites/FavouritesFragment.java
@@ -2,13 +2,22 @@ package ui.favourites;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import com.example.mapped_v6.Favourites;
 import com.example.mapped_v6.R;
+import com.example.mapped_v6.databinding.FragmentFavouritesBinding;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,33 +26,58 @@ import com.example.mapped_v6.R;
  */
 public class FavouritesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private  FavViewModel favViewModel;
+    private FragmentFavouritesBinding binding;
+    private ListView FavLandmarksList;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
+    private Favourites favourites;
+    public static ArrayList<Favourites> favouritesArrayList = new ArrayList<Favourites>();
+    private LinearLayout.LayoutParams params;
+    private LinearLayout lm;
 
     public FavouritesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavouritesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //firebase
+
+        params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        favouriteList();
+    }
+
+    private void favouriteList() {
+        //firebase
+        
+        createList();
+        createClickListen();
+    }
+
+    private void createClickListen() {
+    }
+
+    private void createList() {
+        if(getActivity()!=null) {
+            FavLandmarksList = (ListView) getView().findViewById(R.id.FavLandmarksList);
+
+
+            ////////////////////////
+
+
+
+
+        }
+    }
+
     public static FavouritesFragment newInstance(String param1, String param2) {
         FavouritesFragment fragment = new FavouritesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +86,7 @@ public class FavouritesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
