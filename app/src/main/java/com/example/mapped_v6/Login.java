703fc65d98b754529b7367048b24eab1228cd1de
Tintin8 <tristan071@gmail.com>
2022-10-17 20:34:20 +0200

    ////
    last

--- app/src/main/java/com/example/mapped_v6/Login.java
+++ app/src/main/java/com/example/mapped_v6/Login.java
@@ -1,22 +1,57 @@
 package com.example.mapped_v6;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Locale;
+import java.util.regex.Pattern;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Login#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Login extends Fragment {
+public class Login extends Fragment  {
+
+    private FirebaseAuth mAuth;
+    private String email;
+    private String password;
+    EditText Email;
+    EditText Password;
+    View v;
+    public static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+");
 
 
     public Login() {
@@ -35,6 +70,9 @@ public class Login extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
+
         if (getArguments() != null) {
 
         }
@@ -50,15 +88,100 @@ public class Login extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v =inflater.inflate(R.layout.fragment_login,container,false);
+
+
+         v =inflater.inflate(R.layout.fragment_login,container,false);
         Button demobutton = (Button) v.findViewById(R.id.demobutton);
-        demobutton.setOnClickListener(new View.OnClickListener() {
+        Button btnLogIn=(Button) v.findViewById(R.id.btnLogIn);
+        TextView Signup =(TextView)v.findViewById(R.id.textSignup);
+
+        Signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
+                launchSignup();
             }
         });
+
+
+
+        demobutton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View view) {
+                openLandingPage();
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+
+            }
+        });
+
         return v;
+
+
+
+
     }
+
+    private void launchSignup() {
+
+
+    }
+
+
+    public void signIn(){
+
+
+
+        Email= v.findViewById(R.id.txtUsername);
+        Password=v.findViewById(R.id.txtPassword);
+
+
+
+
+       mAuth = FirebaseAuth.getInstance();
+
+
+      email=Email.getText().toString();
+      password=Password.getText().toString();
+
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Log.d(TAG,"signInWithEmail:success");
+                    Toast.makeText(getActivity(), "22222222", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user=mAuth.getCurrentUser();
+                    updateUI(user);
+                    openLandingPage();
+
+                }
+                else{
+                    Log.w(TAG,"SignWithEmail:Failure",task.getException());// showToast( );
+                    Toast.makeText(getActivity(), "1", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+
+
+
+    }
+
+
+
+    private void openLandingPage() {
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void updateUI(FirebaseUser user) {
+    }
+
 }
