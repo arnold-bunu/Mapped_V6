fb89481391ae9bb1062b1e4ff807425f350fa83d
Carla <carlakearney57@gmail.com>
2022-10-17 14:20:34 +0200

    commit

--- app/src/main/java/com/example/mapped_v6/MainActivity.java
+++ app/src/main/java/com/example/mapped_v6/MainActivity.java
@@ -4,15 +4,29 @@ package com.example.mapped_v6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mapped_v6.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginBtn;
+    Button signupBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
     }
 }
