e2da5b8251e1ccae92770e761af37d969d67800e
Carla <carlakearney57@gmail.com>
2022-10-17 15:19:36 +0200

    commit

--- app/src/main/java/com/example/mapped_v6/Users.java
+++ app/src/main/java/com/example/mapped_v6/Users.java
@@ -1,22 +1,14 @@
 package com.example.mapped_v6;
 
-
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.mapped_v6.R;
-
-public class MainActivity extends AppCompatActivity {
+public class Users extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_users);
     }
 }
