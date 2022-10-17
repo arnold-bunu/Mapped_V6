88a257e05d4d6bf3cde68fcb8045d51267381c65
Tintin8 <tristan071@gmail.com>
2022-10-17 16:14:54 +0200

    commit deez nutz

--- app/src/main/java/com/example/mapped_v6/Login.java
+++ app/src/main/java/com/example/mapped_v6/Login.java
@@ -1,5 +1,6 @@
 package com.example.mapped_v6;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,15 +34,31 @@ public class Login extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
 
         }
     }
 
+
+
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v =inflater.inflate(R.layout.fragment_login,container,false);
+        Button demobutton = (Button) v.findViewById(R.id.demobutton);
+        demobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
