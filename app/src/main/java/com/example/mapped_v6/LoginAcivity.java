70d9cc4cbfd5581f326d5614ae4088d04397e081
Arnold Bunu <arnoldbunu@gmail.com>
2022-10-17 19:22:03 +0200

    credentials done on acivities ICE

---
+++ app/src/main/java/com/example/mapped_v6/LoginAcivity.java
@@ -0,0 +1,14 @@
+package com.example.mapped_v6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acivity);
+    }
+}
